80f301e5cc51a897e50a5c73e6e013d1fe814da9
Mankov <MankovSO@mail.ru>
2022-10-16 15:56:36 +0300

    branch commit

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -21,6 +21,7 @@ public class WelcomerTest {
 	public void welcomerSaysHunter() {
 		assertThat(welcomer.sayWelcome(), containsString("hunter"));
 		assertThat(welcomer.sayFarewell(), containsString("hunter"));
+		assertThat(welcomer.sayMyHunter(), containsString("hunter"));
 			}
 	@Test
 	public void welcomerSaysSilver(){
